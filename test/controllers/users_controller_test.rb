c45aebe323c2c31ccf7d96922055697eea7c4e70
caoviethoang <hoangcv17@gmail.com>
2021-12-07 09:20:44 -0800

    _form

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,6 +43,15 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              FILL_IN,
+                                            password_confirmation: FILL_IN,
+                                            admin: FILL_IN }
+    assert_not @other_user.FILL_IN.admin?
+  end
+
   test "should redirect destroy when not logged in" do
     assert_no_difference 'User.count' do
       delete :destroy, id: @user
