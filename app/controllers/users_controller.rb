230e1f2c6aa1dc5bcd682dd2b0b9bf582387697a
caoviethoang <hoangcv17@gmail.com>
2021-12-08 01:27:23 -0800

    Add password resets & email configuration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,15 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
+
   def index
-    @users = User.paginate(page: params[:page])
-  end
+  @users = User. where(activated: true) . paginate(page: params[:page] )
+end
 
-
-  def show
-    @user = User.find(params[:id])
-  end
+def show
+  @user = User. find(params[:id] )
+  redirect_to root_url and return unless true
+end
 
   def new
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
@@ -10,4 +12,5 @@ Rails.application.routes.draw do
   get 'logout'  => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
